53289c64dbb3b23fb9ac02a748188f09981f7299
xu chen <xuchen@xus-macbook-pro.lan>
2022-06-07 21:47:08 -0700

    [major]homework: week5-day3

--- js/activities.js
+++ js/activities.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
-  $("#activities main tbody tr td:not(:first-child)").each(function() {
+$(document).ready(function () {
+  $("#activities main tbody tr td:not(:first-child)").each(function () {
     if ($(this).text() !== "Not Available") {
-      $(this).css({cursor: "pointer"});
-      $(this).click(function() {
+      $(this).css({ cursor: "pointer" });
+      $(this).click(function () {
         $(this).toggleClass("activitiesCell");
+
+        // week5-day3-lab-home-work
+        if ($(this).hasClass("activitiesCell")) {
+          $("#displaySelected").css("visibility", "visible");
+          $("#displaySelected").css("margin-top", "2em");
+          $("#result").append(
+            "<p>" +
+              $(this).text() +
+              " at " +
+              $("th").eq($(this).index()).text() +
+              "</p>"
+          );
+        } else {
+          $("#result p:contains(" + $(this).text() + ")").remove();
+
+          if (!!!$("#result").has("p").length) {
+            $("#displaySelected").css("visibility", "hidden");
+            $("#displaySelected").css("maigin-top", "0");
+          }
+        }
       });
     }
   });
